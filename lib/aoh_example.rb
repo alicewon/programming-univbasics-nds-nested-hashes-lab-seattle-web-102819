ea747624754c0db5269464491657b3718fab3175
Alice Won <alicewon4@gmail.com>
2019-10-25 23:08:14 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -11,10 +11,10 @@ def assembled_aoh
   # Array. Ruby constants are denoted by ALL_CAPS
 
   array = [
-    {DON_G => {}, 
-    JOELLE_VD => {}, 
-    PAT_M => {}, 
-    KATE_G => {}, 
+    {DON_G => {},
+    JOELLE_VD => {},
+    PAT_M => {},
+    KATE_G => {},
     BRUCE_G => {}
   }
   ]
